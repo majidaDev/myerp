1d4c40e15e739fcf5744efa9c095ddb4c5c28605
majida <majidaibenmansour@gmail.com>
2020-03-21 20:12:45 +0100

    update comment in EcritureComptable

--- myerp-model/src/main/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptable.java
+++ myerp-model/src/main/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptable.java
@@ -99,8 +99,9 @@ public class EcritureComptable {
     public BigDecimal getTotalCredit() {
         BigDecimal vRetour = BigDecimal.ZERO;
         for (LigneEcritureComptable vLigneEcritureComptable : listLigneEcriture) {
-            if (vLigneEcritureComptable.getDebit() != null) {
-                vRetour = vRetour.add(vLigneEcritureComptable.getDebit());
+            // change getDebit to getCredit
+            if (vLigneEcritureComptable.getCredit() != null) {
+                vRetour = vRetour.add(vLigneEcritureComptable.getCredit());
             }
         }
         return vRetour;
@@ -111,7 +112,9 @@ public class EcritureComptable {
      * @return boolean
      */
     public boolean isEquilibree() {
-        boolean vRetour = this.getTotalDebit().equals(getTotalCredit());
+        //add compareTo for BigDecimal and not Equals
+       int  vRetourBigDecimal = this.getTotalDebit().compareTo(getTotalCredit());
+        boolean vRetour = vRetourBigDecimal == 0;
         return vRetour;
     }
 
