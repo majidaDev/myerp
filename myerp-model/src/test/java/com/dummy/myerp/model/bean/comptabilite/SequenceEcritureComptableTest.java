89382dce3071a1d453c75b5e93b2fdb7859abd63
majida <majidaibenmansour@gmail.com>
2020-03-21 19:59:44 +0100

    unit test SequenceEcritureComptable

---
+++ myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/SequenceEcritureComptableTest.java
@@ -0,0 +1,33 @@
+package com.dummy.myerp.model.bean.comptabilite;
+
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class SequenceEcritureComptableTest {
+
+    private SequenceEcritureComptable sequenceEcritureComptable;
+
+    @Before
+    public void setUp() {
+        sequenceEcritureComptable = new SequenceEcritureComptable();
+        sequenceEcritureComptable.setAnnee(2019);
+        sequenceEcritureComptable.setDerniereValeur(1000);
+
+        //with constructor
+        sequenceEcritureComptable = new SequenceEcritureComptable(2019, 1000);
+    }
+
+    @Test
+    public void testToString() {
+        StringBuilder vStB = new StringBuilder("SequenceEcritureComptable");
+        String vSEP = ", ";
+        vStB.append("{")
+                .append("annee=").append(sequenceEcritureComptable.getAnnee())
+                .append(vSEP).append("derniereValeur=").append(sequenceEcritureComptable.getDerniereValeur())
+                .append("}");
+        Assert.assertEquals(vStB.toString(),sequenceEcritureComptable.toString());
+    }
+}
